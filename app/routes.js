2e3dd21a9ad0b70ef186f1b17ef855add81d19e5
jolumley <49235579+jolumley@users.noreply.github.com>
2022-10-13 15:01:46 +0100

    HRT PPC

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,39 @@
 const express = require('express');
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
+// HRT PPC Routes //
+
+router.get(/ppcEmail/, function (req, res) {
+    if (req.query.radioInlineGroup === "Email" ) {
+      res.redirect('ppc-customer-email1');
+      } else {
+      res.redirect('ppc-cya');
+    }
+  });
+  
+  router.get(/hrtPpcEmail/, function (req, res) {
+    if (req.query.radioInlineGroup === "Email" ) {
+      res.redirect('hrtppc-customer-email1');
+      } else {
+      res.redirect('hrtppc-cya');
+    }
+  });
+  
+  router.get(/hrtHrtEmail/, function (req, res) {
+    if (req.query.radioInlineGroup === "Email" ) {
+      res.redirect('hrt-customer-email1');
+      } else {
+      res.redirect('hrt-cya');
+    }
+  });
+  
+  router.get(/ppcHrtQuestion/, function (req, res) {
+    if (req.query.radioInlineGroup === "Yes" ) {
+      res.redirect('ppc-hrt-ppc-customer-name');
+      } else {
+      res.redirect('hrt-customer-name');
+    }
+  });
+  
 
 module.exports = router;
