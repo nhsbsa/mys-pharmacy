125125faa1f435a4353eb83adcc015620c6dee35
kellyleung1 <kelly.leung1@nhsbsa.nhs.uk>
2023-10-10 14:55:47 +0100

    Covid express of interest

--- app/routes.js
+++ app/routes.js
@@ -528,7 +528,7 @@ router.post(/ActivitiesClaim/, function (req, res) {
   if (ActivitiesClaim === "yes") {
     res.redirect('Summaryv3');
   } else {
-    res.redirect('');
+    res.redirect('advanced-servicesv3');
   }
 });
 
